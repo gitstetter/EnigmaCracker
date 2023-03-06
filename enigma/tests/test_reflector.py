7507b1c71359eb22620765a8ad2a223fff0c4faa
gitstetter <a.fussstetter@gmx.net>
2023-03-06 20:18:25 +0100

    added unittests for reflectors

--- enigma/tests/test_reflector.py
+++ enigma/tests/test_reflector.py
@@ -1,3 +1,18 @@
-import pytest
+import sys
+sys.path.append('.')
+
+
+
 from enigma.reflector import *
-print("x")
+
+def test_reflectorA_encipherZ():
+    x = REFLECTOR_A.encipher(key="z")
+    assert x == 'D'
+
+def test_reflectorB_encipherZ():
+    x = REFLECTOR_B.encipher(key="z")
+    assert x == 'T'
+
+def test_reflectorC_encipherZ():
+    x = REFLECTOR_C.encipher(key="z")
+    assert x == 'L'
