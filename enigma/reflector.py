111f984d9d2f811ab6e4ca7fa06eaebdda9932fb
gitstetter <a.fussstetter@gmx.net>
2023-03-06 18:55:49 +0100

    added reflectors

---
+++ enigma/reflector.py
@@ -0,0 +1,38 @@
+class Reflector:
+    """
+    Represents a reflector which directs current back through thr rotors
+    """
+
+    def __init__(self, wiring=None, name=None):
+        if wiring != None:
+            self.wiring = wiring
+        else:
+            self.wiring =  "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        self.name = name
+
+    
+    def __setattr__(self, name: str, value) -> None:
+        self.__dict__[name] = value
+
+    def __eq__(self, rotor) -> bool:
+        return self.name == rotor.name
+
+    def encipher(self, key: str):
+        assert type(key) == str
+        assert len(key) == 1
+        key = key.upper()
+        assert key in "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        index = (ord(key) - ord("A"))
+        letter = self.wiring[index]
+        # print("letter: "+letter)
+        return letter
+
+
+
+REFLECTOR_A = Reflector(wiring="EJMZALYXVBWFCRQUONTSPIKHGD", name="REFLECTOR_A")
+REFLECTOR_B = Reflector(wiring="YRUHQSLDPXNGOKMIEBFZCWVJAT", name="REFLECTOR_B")
+REFLECTOR_C = Reflector(wiring="FVPJIAOYEDRZXWGCTKUQSBNMHL", name="REFLECTOR_C")
+
+# REFLECTOR_A.encipher(key="z")
+# REFLECTOR_B.encipher(key="z")
+# REFLECTOR_C.encipher(key="z")
