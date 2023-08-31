72d0b0d788496ac058638295e085993e1a78f70f
gitstetter <a.fussstetter@gmx.net>
2023-08-31 19:19:43 +0200

    first commit

--- libs/enigma/rotor.py
+++ libs/enigma/rotor.py
@@ -5,15 +5,15 @@ class Rotor:
     Represents a Rotor that maps characters pairwise and turns the rotor to the right notches
     """
 
-    def __init__(self, wiring:str=None, name:str=None, ring_position:str="1", rotor_position:str='A', notch_position:str=None):
+    def __init__(self, wiring:str=None, name:str=None, ring_position:int=1, rotor_position:int=1, notch_position:str=None):
         if wiring is not None:
             self.wiring = wiring
         else:
             self.wiring = UPPERCASE_LETTERS
         self.name = name
         self.notch_position = notch_position.upper()
-        self.ring_position = int(ring_position)
-        self.rotor_position = rotor_position.upper()
+        self.ring_position = ring_position
+        self.rotor_position = rotor_position
         
         self.rev_wiring = ["0"] * 26
         for i in range(0, len(self.wiring)):
@@ -39,7 +39,10 @@ class Rotor:
     def handle_ring_setting(self) -> None:
         assert type(self.ring_position) is int
         assert self.ring_position < 27
-        self.rotor_position = chr((ord(self.rotor_position) + self.ring_position-1 - ord("A")) % 26 + ord("A"))
+        # self.rotor_position = (self.rotor_position + self.ring_position) % 26
+        self.rotor_position = ((self.rotor_position + self.ring_position-1) % 26 
+                               if (self.rotor_position + self.ring_position) <=26 
+                               else (self.rotor_position + self.ring_position) % 26 )
 
 
     def encipher_forward(self, key: str) -> str:
@@ -49,8 +52,8 @@ class Rotor:
         assert key in UPPERCASE_LETTERS
         index = (ord(key) - ord("A"))
         #Account for current rotor position
-        rotor_shift = ord(self.rotor_position)%65
-        index = (index + rotor_shift)%26
+        rotor_shift = self.rotor_position
+        index = (index + rotor_shift-1)%26
         letter = self.wiring[index]
         return letter
 
@@ -62,22 +65,22 @@ class Rotor:
         assert key in UPPERCASE_LETTERS
         index = (ord(key) - ord("A"))
         #Account for current rotor position
-        rotor_shift = ord(self.rotor_position)%65
-        index = (index + rotor_shift)%26
+        rotor_shift = self.rotor_position
+        index = (index + rotor_shift-1)%26
         letter = self.rev_wiring[index]
         return letter
 
     
     def notch(self) -> None:
-        self.rotor_position = chr((ord(self.rotor_position) + 1 - ord("A")) % 26 + ord("A"))
+        self.rotor_position = self.rotor_position + 1 if self.rotor_position <26 else 1
 
 
     def is_in_turnover_pos(self) -> bool:
-        return self.rotor_position == self.notch_position
+        return self.rotor_position == ord(self.notch_position) - ord("A")
 
 
-ROTOR_I = Rotor(wiring="EKMFLGDQVZNTOWYHXUSPAIBRCJ", name="Rotor_I", ring_position=1, rotor_position='A', notch_position='R')
-ROTOR_II = Rotor(wiring="AJDKSIRUXBLHWTMCQGZNPYFVOE", name="Rotor_II", ring_position=1, rotor_position='A', notch_position='F')
-ROTOR_III = Rotor(wiring="BDFHJLCPRTXVZNYEIWGAKMUSQO", name="Rotor_III", ring_position=1, rotor_position='A', notch_position='W')
-ROTOR_IV = Rotor(wiring="ESOVPZJAYQUIRHXLNFTGKDCMWB", name="Rotor_IV", ring_position=1, rotor_position='A', notch_position='K')
-ROTOR_V = Rotor(wiring="VZBRGITYUPSDNHLXAWMJQOFECK", name="Rotor_V", ring_position=1, rotor_position='A', notch_position='A')
+ROTOR_I = Rotor(wiring="EKMFLGDQVZNTOWYHXUSPAIBRCJ", name="Rotor_I", ring_position=1, rotor_position=1, notch_position='R')
+ROTOR_II = Rotor(wiring="AJDKSIRUXBLHWTMCQGZNPYFVOE", name="Rotor_II", ring_position=1, rotor_position=1, notch_position='F')
+ROTOR_III = Rotor(wiring="BDFHJLCPRTXVZNYEIWGAKMUSQO", name="Rotor_III", ring_position=1, rotor_position=1, notch_position='W')
+ROTOR_IV = Rotor(wiring="ESOVPZJAYQUIRHXLNFTGKDCMWB", name="Rotor_IV", ring_position=1, rotor_position=1, notch_position='K')
+ROTOR_V = Rotor(wiring="VZBRGITYUPSDNHLXAWMJQOFECK", name="Rotor_V", ring_position=1, rotor_position=1, notch_position='A')
--- libs/enigma/tests/test_rotor.py
+++ libs/enigma/tests/test_rotor.py
@@ -3,17 +3,17 @@ import pytest
 
 
 def test_ROTORI_encipher_forward():
-    ROTOR_I.rotor_position = 'A'
+    ROTOR_I.rotor_position = 1
     assert ROTOR_I.encipher_forward('B')=='K'
 
 def test_ROTORI_encipher_forward_with_notch():
-    ROTOR_I.rotor_position = 'A'
+    ROTOR_I.rotor_position = 1
     assert ROTOR_I.encipher_forward('B')=='K'
     ROTOR_I.notch()
     assert ROTOR_I.encipher_forward('B')=='M'
 
 def test_ROTORI_encipher_forward_with_notch_over():
-    ROTOR_I.rotor_position = 'Y'
+    ROTOR_I.rotor_position = 25
     assert ROTOR_I.encipher_forward('B')=='J'
     ROTOR_I.notch()
     assert ROTOR_I.encipher_forward('B')=='E'
@@ -21,18 +21,18 @@ def test_ROTORI_encipher_forward_with_notch_over():
     assert ROTOR_I.encipher_forward('B')=='K'   
 
 def test_ROTORI_encipher_backwards():
-    ROTOR_I.rotor_position = 'A'
+    ROTOR_I.rotor_position = 1
     assert ROTOR_I.encipher_backwards('K')=='B' 
 
 def test_ROTORI_encipher_backwards_with_notch():
-    ROTOR_I.rotor_position = 'A'
+    ROTOR_I.rotor_position = 1
     assert ROTOR_I.encipher_backwards('K')=='B'
     ROTOR_I.notch()
     assert ROTOR_I.encipher_backwards('K')=='E'
     assert ROTOR_I.encipher_backwards('M')=='K'
 
 def test_ROTORI_encipher_backwards_with_notch_over():
-    ROTOR_I.rotor_position = 'Y'
+    ROTOR_I.rotor_position = 25
     assert ROTOR_I.encipher_backwards('B')=='J'
     ROTOR_I.notch()
     assert ROTOR_I.encipher_backwards('B')=='U'
@@ -56,68 +56,71 @@ def test_ROTORI_encipher_back_two_str():
         ROTOR_I.encipher_backwards('AA')
 
 def test_ROTORI_notch():
-    ROTOR_I.rotor_position = 'A'
+    ROTOR_I.rotor_position = 1
     ROTOR_I.notch()
-    assert ROTOR_I.rotor_position == 'B'
+    assert ROTOR_I.rotor_position == 2
     ROTOR_I.notch()
-    assert ROTOR_I.rotor_position == 'C'
+    assert ROTOR_I.rotor_position == 3
 
 def test_ROTORI_notch_over():
-    ROTOR_I.rotor_position = 'Y'
+    ROTOR_I.rotor_position = 25
     ROTOR_I.notch()
-    assert ROTOR_I.rotor_position == 'Z'
+    assert ROTOR_I.rotor_position == 26
     ROTOR_I.notch()
-    assert ROTOR_I.rotor_position == 'A'
+    assert ROTOR_I.rotor_position == 1
     ROTOR_I.notch()
-    assert ROTOR_I.rotor_position == 'B'
+    assert ROTOR_I.rotor_position == 2
 
+#Test rings
 def test_ROTORI_ringsetting_1():
+    ROTOR_I.rotor_position = 1
     ROTOR_I.ring_position = 1
-    ROTOR_I.rotor_position = 'A'
     ROTOR_I.handle_ring_setting()
-    assert ROTOR_I.rotor_position == 'A'
+    assert ROTOR_I.rotor_position == 1
 
 def test_ROTORI_ringsetting_2():
+    ROTOR_I.rotor_position = 1
     ROTOR_I.ring_position = 2
-    ROTOR_I.rotor_position = 'A'
     ROTOR_I.handle_ring_setting()
-    assert ROTOR_I.rotor_position == 'B'
+    assert ROTOR_I.rotor_position == 2
+
 
 def test_ROTORI_ringsetting_26():
+    ROTOR_I.rotor_position = 1
     ROTOR_I.ring_position = 26
-    ROTOR_I.rotor_position = 'A'
     ROTOR_I.handle_ring_setting()
-    assert ROTOR_I.rotor_position == 'Z'
+    assert ROTOR_I.rotor_position == 26
 
 def test_ROTORI_ringsetting_notch_over():
     ROTOR_I.ring_position = 3
-    ROTOR_I.rotor_position = 'Y'
+    ROTOR_I.rotor_position = 25
     ROTOR_I.handle_ring_setting()
-    assert ROTOR_I.rotor_position == 'A'
+    assert ROTOR_I.rotor_position == 1
+
+
+def test_ROTORI_ringsetting_encipher_forward():
+    ROTOR_I.rotor_position = 1
+    ROTOR_I.set_ring_position(2)
+    assert ROTOR_I.rotor_position==2
+    assert ROTOR_I.encipher_forward('K')=='T'
+    ROTOR_I.rotor_position = 1
+    ROTOR_I.set_ring_position(26)
+    assert ROTOR_I.encipher_forward('Z')=='C'
 
 def test_ROTORI_ringsetting_invalid_int():
     with pytest.raises(AssertionError):
         ROTOR_I.ring_position = 27
-        ROTOR_I.rotor_position = 'Y'
+        ROTOR_I.rotor_position = 25
         ROTOR_I.handle_ring_setting()
-        assert ROTOR_I.rotor_position == 'A'
+        assert ROTOR_I.rotor_position == 1
 
 def test_ROTORI_ringsetting_invalid_type():
     with pytest.raises(AssertionError):
         ROTOR_I.ring_position = 'Not supported'
-        ROTOR_I.rotor_position = 'Y'
+        ROTOR_I.rotor_position = 25
         ROTOR_I.handle_ring_setting()
-        assert ROTOR_I.rotor_position == 'A'
-
-def test_ROTORI_ringsetting_encipher_forward():
-    ROTOR_I.rotor_position = 'A'
-    ROTOR_I.set_ring_position(2)
-    assert ROTOR_I.rotor_position=='B'
-    assert ROTOR_I.encipher_forward('K')=='T'
-    ROTOR_I.rotor_position = 'A'
-    ROTOR_I.set_ring_position(26)
-    assert ROTOR_I.encipher_forward('Z')=='C'
+        assert ROTOR_I.rotor_position == 1
 
 def test_ROTORI_is_at_notch():
-    ROTOR_I.rotor_position = ROTOR_I.notch_position
+    ROTOR_I.rotor_position = ord(ROTOR_I.notch_position)-ord("A")
     assert ROTOR_I.is_in_turnover_pos()
