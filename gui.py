11f7204320a4749b20f9ce4fb9b03e6cb2e2c789
gitstetter <a.fussstetter@gmx.net>
2023-11-20 18:09:16 +0100

    added Plugboard to gui

--- gui.py
+++ gui.py
@@ -7,7 +7,7 @@ from libs.enigma.reflector import REFLECTOR_A, REFLECTOR_B, REFLECTOR_C
 from libs.enigma.enigma import Enigma
 from libs.enigma.plugboard import Plugboard
 
-logging.basicConfig(filename='log.txt',
+logging.basicConfig(filename='gui.log',
                     filemode='w',
                     format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
                     # datefmt='%H:%M:%S',
@@ -39,14 +39,16 @@ def set_enigma_params():
     left_rotor=cbox_left_rotor.get()
     middle_rotor=cbox_middle_rotor.get()
     right_rotor=cbox_right_rotor.get()
+
     left_rotor_pos=cbox_left_rotor_pos.get()
     middle_rotor_pos=cbox_middle_rotor_pos.get()
     right_rotor_pos=cbox_right_rotor_pos.get()
+    logging.info("middle rotor: "+ middle_rotor_pos)
+
     left_rotor_ring=cbox_left_rotor_ring.get()
     middle_rotor_ring=cbox_middle_rotor_ring.get()
     right_rotor_ring=cbox_right_rotor_ring.get()
-    logging.info(f"Selected settings are: {reflector}, {left_rotor}, {left_rotor_pos}, {left_rotor_ring}")
-
+    logging.info(f"Settings of EnigmaMachine were changed")
     logging.info("Before: "+str(EnigmaMachine))
     EnigmaMachine.reflector = REFLECTOR_MAP[reflector]
     EnigmaMachine.left_rotor = ROTOR_MAP[left_rotor]
@@ -58,6 +60,8 @@ def set_enigma_params():
     EnigmaMachine.right_rotor = ROTOR_MAP[right_rotor]
     EnigmaMachine.right_rotor.rotor_position = int(right_rotor_pos)
     EnigmaMachine.right_rotor.ring_position = int(right_rotor_ring)
+    plugsettings=txt_plugs.get("1.0", tk.END).replace("\n", "").strip()
+    EnigmaMachine.plugboard = Plugboard(plugsettings)
     logging.info("After: "+str(EnigmaMachine))
 
 window = tk.Tk()
@@ -70,11 +74,9 @@ window.rowconfigure(1, minsize=50, weight=1)
 
 frm_menu = tk.Frame(window, relief=tk.RAISED, bd=1)
 frm_text = tk.Frame(window)
-
-frm_enigma_setup = tk.Frame(frm_menu, relief=tk.RAISED, bd=1)
 frm_encrypt_out = tk.Frame(window)
 
-
+frm_enigma_setup = tk.Frame(frm_menu, relief=tk.RAISED, bd=1)
 btn_encipher=tk.Button(frm_menu, text="encrypt!", command=set_lbl_encrypt_out)
 btn_save=tk.Button(frm_menu, text="save!", command=set_enigma_params)
 
@@ -101,35 +103,44 @@ cbox_middle_rotor_ring.current(0)
 cbox_right_rotor_ring = ttk.Combobox(frm_menu, values=[x for x in range(1, 27)],state="readonly", width=3)
 cbox_right_rotor_ring.current(0)
 
-lbl_pos = tk.Label(frm_menu, text="Rotor Position")
-lbl_ring = tk.Label(frm_menu, text="Ring Position")
+txt_plugs = tk.Text(frm_menu, width=29, height=1)
+txt_plugs.insert(1.0, "AA BB CC")
 
-txt_plain = tk.Text(frm_text)
+
+lbl_pos = tk.Label(frm_menu, text="Rotor Position:", justify="left")
+lbl_ring = tk.Label(frm_menu, text="Ring Position: ", justify="left")
+lbl_plugs = tk.Label(frm_menu, text="Plug Settings:", justify="left")
+
+txt_plain = tk.Text(frm_text, height=10)
+
 lbl_encrypt_out = tk.Label(frm_encrypt_out, text="Encrypt your message above")
 
 
 frm_enigma_setup.grid(row=0, column=0, sticky="n") #force the frame to expand vertically
 
-cbox_reflector.grid(row=0, column=1)
-cbox_left_rotor.grid(row=0, column=2)
-cbox_middle_rotor.grid(row=0, column=3)
-cbox_right_rotor.grid(row=0, column=4)
+cbox_reflector.grid(row=0, column=0)
+cbox_left_rotor.grid(row=0, column=1)
+cbox_middle_rotor.grid(row=0, column=2)
+cbox_right_rotor.grid(row=0, column=3)
 
-cbox_left_rotor_pos.grid(row=1, column=2)
-cbox_middle_rotor_pos.grid(row=1, column=3)
-cbox_right_rotor_pos.grid(row=1, column=4)
+cbox_left_rotor_pos.grid(row=1, column=1)
+cbox_middle_rotor_pos.grid(row=1, column=2)
+cbox_right_rotor_pos.grid(row=1, column=3)
 
-cbox_left_rotor_ring.grid(row=2, column=2)
-cbox_middle_rotor_ring.grid(row=2, column=3)
-cbox_right_rotor_ring.grid(row=2, column=4)
+cbox_left_rotor_ring.grid(row=2, column=1)
+cbox_middle_rotor_ring.grid(row=2, column=2)
+cbox_right_rotor_ring.grid(row=2, column=3)
 
-lbl_pos.grid(row=1, column=1)
-lbl_ring.grid(row=2, column=1)
+txt_plugs.grid(row=3, columnspan=3, column=1)
 
-btn_encipher.grid(row=1, column=5, sticky="ns", padx=5, pady=5) #expand horizontally in both directions and fill the entire frame
-btn_save.grid(row=0, column=5, sticky="ns", padx=5, pady=5) #expand horizontally in both directions and fill the entire frame
-txt_plain.grid(row=1, column=0, sticky="ns") #force the frame to expand in every direction.
-lbl_encrypt_out.grid(row=1, column=1)
+lbl_pos.grid(row=1, column=0)
+lbl_ring.grid(row=2, column=0)
+lbl_plugs.grid(row=3, column=0)
+
+btn_save.grid(row=4, column=3, sticky="ns", padx=5, pady=5) #expand horizontally in both directions and fill the entire frame
+btn_encipher.grid(row=4, column=4, sticky="ns", padx=5, pady=5) #expand horizontally in both directions and fill the entire frame
+txt_plain.grid(row=1, column=0, sticky="nsew") #force the frame to expand in every direction.
+lbl_encrypt_out.grid(row=1, column=1, sticky="nsew")
 
 frm_menu.grid(row=0, column=1)#, sticky="ns") #force the frame to expand vertically
 frm_text.grid(row=0, column=2)#, sticky="ns")
