232d1add27469a187e5721b42dd92b689792cc8d
gitstetter <a.fussstetter@gmx.net>
2024-02-25 18:36:08 +0100

    renaming type hints ioc

--- libs/enigmacracker/metrics/ioc.py
+++ libs/enigmacracker/metrics/ioc.py
@@ -1,33 +1,33 @@
-def calculate_ioc(text):
-    # Function to calculate the Index of Coincidence (IoC)
-    
-    # Convert the text to lowercase
-    text = text.lower()
+def calculate_ioc(text:str)->float:
+    """
+    Function to calculate the Index of Coincidence (IoC)
+    """
+    text:str = text.lower()
     
     # Initialize a dictionary to store letter frequencies
-    frequencies = {chr(i): 0 for i in range(ord('a'), ord('z') + 1)}
+    frequencies:dict = {chr(i): 0 for i in range(ord('a'), ord('z') + 1)}
     
     # Count the occurrences of each letter
-    total_letters = 0  # Variable to count total letters in the text
+    total_letters:int = 0  # Variable to count total letters in the text
     for char in text:
-        if char.isalpha():  # Only consider alphabetical characters
+        if char.isalpha():  # Only consider alphabetical characters, no whitespace
             frequencies[char] += 1
             total_letters += 1
     
     # Calculate the numerator of the IoC formula
-    numerator = sum(frequencies[char] * (frequencies[char] - 1) for char in frequencies.keys())
+    numerator:int = sum(frequencies[char] * (frequencies[char] - 1) for char in frequencies.keys())
     
     # Calculate the denominator of the IoC formula
-    denominator = total_letters * (total_letters - 1)
+    denominator:int = total_letters * (total_letters - 1)
     
     # Calculate the IoC
-    ioc = numerator / denominator
+    ioc:float = numerator / denominator
     
     return ioc
 
 def main():
     text_ciphered = "BKREVE BMLVYMAO QCY RC YESNRP PE LXWLQCAEBJ ZOT ACWISMP XGQ UKBDCFZC BFH KPRU VGCSMLDH ZFS DMC GQBY VFVRFXEP YYGGSDLTVBCD"
-    text = 'Enigma messages can be solved by recovering the message key settings the ring settings and the plug settings individually'.upper()
+    text = 'Enigma messages can be solved by recovering the message key settings the ring settings and the plug settings individually'
     ioc = calculate_ioc(text)
     print("Index of Coincidence:", ioc)
     ioc = calculate_ioc(text_ciphered)
