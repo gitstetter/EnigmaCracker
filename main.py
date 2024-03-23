79345ce34c41ce7e7278c2999d0c6a0b4f2108b5
gitstetter <a.fussstetter@gmx.net>
2024-03-23 10:18:56 +0100

    renamed LICENSE

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ EnigmaMachine2 = Enigma(reflector=REFLECTOR_B,
                        rotor_positions="16 10 1",
                        ring_positions="3 1 17",
                        plugboard=Plugboard("CE KL FP MD")
-                     #   plugboard=Plug1board()
+                     #   plugboard=Plugboard()
                        )
 
 x=EnigmaMachine2.encipher('Jeder Mensch sollte wissen, wie man Enigma-Maschine verwendet. Es ist ein faszinierendes Stueck Geschichte und Technologie. Durch das Studium der Enigma konnen wir die Komplexitaet und den Einfallsreichtum vergangener Generationen besser verstehen. Die Maschine wurde im Zweiten Weltkrieg intensiv genutzt und spielte eine entscheidende Rolle bei der Entschluesselung von Nachrichten. Ihr Einfluss auf die moderne Kryptografie ist nicht zu unterschaetzen. Das Geheimnis der Enigma hat die Neugierde und den Forschungsgeist vieler Menschen auf der ganzen Welt geweckt. Es ist ein Symbol fur den unermuedlichen Drang des Menschen nach Wissen und Innovation. Moge ihr Erbe weiterleben und uns dazu inspirieren, die Geheimnisse der Welt zu erforschen.')
